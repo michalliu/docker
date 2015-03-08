e4fae0932b64d4de15fc0f56dc8f02eb96afe499
root <root@OpenWrt.lan>
2015-03-08 05:46:14 +0000

    add alternative build script

---
+++ build-binary-dryrun.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+SCRIPT=/tmp/build-binary-dryrun.log
+RUN=run.sh
+if [ ! -f $RUN ];then
+./hack/make.sh binary-dryrun 2>&1 | tee $SCRIPT > /dev/null 2>&1
+sed -i -e 1,13d $SCRIPT
+(cat << EOF; cat $SCRIPT) > $RUN
+set -e
+set -v
+. envsetup.sh
+WORK=/tmp/go-docker
+mkdir -p bundles/1.3.1/binary-dryrun
+EOF
+chmod +x $RUN
+fi
+./$RUN 2>&1 | tee run.log
